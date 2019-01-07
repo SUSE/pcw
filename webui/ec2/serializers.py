2dee2a9a23cd75b9580dd1e2d5b523d5490f4571
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2019-01-07 19:05:06 +0100

    Cleanup code to pass flake8

--- webui/ec2/serializers.py
+++ webui/ec2/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 
+
 class AccessKeySerializer(serializers.Serializer):
     key_id = serializers.CharField(max_length=128)
-#    status = serializers.CharField(choices = ['active', 'inaktive'])
     status = serializers.CharField(max_length=128)
     secret = serializers.CharField(max_length=128)
     create_date = serializers.DateTimeField()
+
 
 class UserSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=32)
     id = serializers.CharField(max_length=128)
     create_date = serializers.DateTimeField()
     keys = AccessKeySerializer(many=True)
-
-
