daa5c66f5d4c72c2c7e6cae44d56350746613b31
Anton Smorodskyi <asmorodskyi@suse.com>
2019-01-08 22:51:55 +0100

    EC2: Add constructors with parameters for User and AccessKey

--- webui/ec2/models.py
+++ webui/ec2/models.py
@@ -5,9 +5,21 @@ class User(object):
     create_date = None
     keys = []
 
+    def __init__(self, name=None, id=None, create_date=None):
+        self.name = name
+        self.id = id
+        self.create_date = create_date
+        self.keys = []
+
 
 class AccessKey(object):
     key_id = None
     status = None
     create_date = None
     secret = None
+
+    def __init__(self, key_id=None, status=None, create_date=None):
+        self.key_id = key_id
+        self.status = status.lower()
+        self.create_date = create_date
+        self.secret = None
