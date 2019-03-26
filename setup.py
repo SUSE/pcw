1185ea8cff23efdc2ef7b738f96010c1aee0a1e4
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2019-03-26 22:45:20 +0100

    Add missing dependency's to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
         install_requires=[
             'boto3',
             'azure',
+            'uwsgi',
+            'requests',
             'django',
             'django-tables2',
             'django-filter',
