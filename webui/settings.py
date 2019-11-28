289c2e69af2f56da25d23408ab6b331720a7f867
Anton Smorodskyi <asmorodskyi@suse.com>
2019-11-28 16:13:06 +0100

    Move database to memory to ease use in docker

--- webui/settings.py
+++ webui/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'webui.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': ':memory:',
     }
 }
 
