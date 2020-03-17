SUSE/pcw

commit c84755e9a4122a5c3fc40c17d474b2e0f8d5042c

Fix: Delete disk and images from upload resource group
With this patch, we cleanup the disk and image resources from
a resource group specified with azure-storage-resourcegroup in pcw.ini

We now have a common function in Provider called
`get_keeping_image_names()` which get a list of `Images` and makes the
decision which images can stay, which not.

ocw/lib/EC2.py
@@ -1,6 +1,5 @@
-from .provider import Provider
+from .provider import Provider, Image
 from .vault import EC2Credential
-from distutils.version import LooseVersion
 from dateutil.parser import parse
 import boto3
 import re
@@ -122,33 +121,21 @@ class EC2(Provider):
 
     def cleanup_all(self):
         response = self.ec2_client().describe_images(Owners=['self'])
-        images = dict()
+        images = list()
         for img in response['Images']:
             # img is in the format described here:
             # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.describe_images
             m = self.parse_image_name(img['Name'])
             if m:
-                key = m['key']
-                if key not in images:
-                    images[key] = list()
-
                 logger.debug("[{}]Image {} is candidate for deletion with build {}".format(
                     self.__credentials.namespace, img['Name'], m['build']))
-                images[key].append({
-                    'build': m['build'],
-                    'name': img['Name'],
-                    'creation_datetime':  parse(img['CreationDate']),
-                    'id': img['ImageId'],
-                })
+                images.append(Image(img['Name'], flavor=m['key'], build=m['build'], date=parse(img['CreationDate']),
+                                    img_id=img['id']))
             else:
                 logger.error("[{}] Unable to parse image name '{}'".format(self.__credentials.namespace, img['Name']))
 
-        for key in images:
-            images[key].sort(key=lambda x: LooseVersion(x['build']), reverse=True)
+        keep_images = self.get_keeping_image_names(images)
 
-        for img_list in images.values():
-            for i in range(0, len(img_list)):
-                img = img_list[i]
-                if (self.needs_to_delete_image(i, img['creation_datetime'])):
-                    logger.info("[EC2] Delete image '{}' (ami:{})".format(img['name'], img['id']))
-                    self.ec2_client().deregister_image(ImageId=img['id'], DryRun=False)
+        for img in [i for i in images if i.name not in keep_images]:
+            logger.info("Delete image '{}' (ami:{})".format(img.name, img.id))
+            self.ec2_client().deregister_image(ImageId=img.id, DryRun=False)

tests/test_ec2.py
@@ -1,5 +1,9 @@
 from ocw.lib.EC2 import EC2
+from ocw.lib.provider import Provider
 from webui.settings import ConfigFile
+from tests.generators import mock_cfgGet
+from tests.generators import min_image_age_hours, max_image_age_hours
+from datetime import datetime, timezone, timedelta
 
 
 def test_parse_image_name(monkeypatch):
@@ -31,3 +35,53 @@ def test_parse_image_name(monkeypatch):
             }
 
     assert ec2.parse_image_name('do not match') is None
+
+
+def test_cleanup_all(monkeypatch):
+    newer_then_min_age = datetime.now(timezone.utc).isoformat()
+    older_then_min_age = (datetime.now(timezone.utc) - timedelta(hours=min_image_age_hours+1)).isoformat()
+    older_then_max_age = (datetime.now(timezone.utc) - timedelta(hours=max_image_age_hours+1)).isoformat()
+
+    response = {
+            'Images': [
+                {'Name': 'SomeThingElse',
+                    'CreationDate': older_then_max_age, 'id': 0},
+                {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.10.raw.xz',
+                    'CreationDate': newer_then_min_age, 'id': 1},
+                {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.11.raw.xz',
+                    'CreationDate': older_then_min_age, 'id': 2},
+                {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.12.raw.xz',
+                    'CreationDate': older_then_min_age, 'id': 3},
+                {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.13.raw.xz',
+                    'CreationDate': older_then_max_age, 'id': 4},
+                ]
+            }
+    deleted_images = list()
+
+    def mocked_ec2_client():
+        pass
+    mocked_ec2_client.describe_images = lambda *args, **kwargs: response
+    mocked_ec2_client.deregister_image = lambda *args, **kwargs: deleted_images.append(kwargs['ImageId'])
+
+    monkeypatch.setattr(Provider, 'cfgGet', mock_cfgGet)
+    monkeypatch.setattr(EC2, 'check_credentials', lambda *args, **kwargs: True)
+    monkeypatch.setattr(EC2, 'ec2_client', lambda self: mocked_ec2_client)
+
+    ec2 = EC2('fake')
+    ec2.cleanup_all()
+
+    assert deleted_images == [2, 3, 4]
+
+    deleted_images = list()
+    response = {
+            'Images': [
+                {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.12.raw.xz',
+                    'CreationDate': older_then_min_age, 'id': 3},
+                {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.13.raw.xz',
+                    'CreationDate': older_then_min_age, 'id': 4},
+                ]
+            }
+    ec2.cleanup_all()
+    assert deleted_images == [3]
+
+

ocw/lib/azure.py
@@ -1,4 +1,4 @@
-from .provider import Provider
+from .provider import Provider, Image
 from ..lib.vault import AzureCredential
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.resource import ResourceManagementClient
@@ -6,7 +6,6 @@ from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.storage import StorageManagementClient
 from azure.storage.blob import BlockBlobService
 from msrest.exceptions import AuthenticationError
-from distutils.version import LooseVersion
 import re
 import time
 import logging
@@ -67,6 +66,14 @@ class Azure(Provider):
                 self.sp_credentials(), self.subscription())
         return self.__resoure_mgmt_client
 
+    def get_storage_key(self):
+        resourcegroup = self.cfgGet('cleanup', 'azure-storage-resourcegroup')
+        storage_account = self.cfgGet('cleanup', 'azure-storage-account-name')
+        storage_client = StorageManagementClient(self.sp_credentials(), self.subscription())
+        storage_keys = storage_client.storage_accounts.list_keys(resourcegroup, storage_account)
+        storage_keys = [v.value for v in storage_keys.keys]
+        return storage_keys[0]
+
     def list_instances(self):
         return [i for i in self.compute_mgmt_client().virtual_machines.list_all()]
 
@@ -76,22 +83,48 @@ class Azure(Provider):
     def delete_resource(self, resource_id):
         return self.resource_mgmt_client().resource_groups.delete(resource_id)
 
+    def list_images_by_resource_group(self, resource_group):
+        return self.list_by_resource_group(resource_group,
+                                           filters="resourceType eq 'Microsoft.Compute/images'")
+
+    def list_disks_by_resource_group(self, resource_group):
+        return self.list_by_resource_group(resource_group,
+                                           filters="resourceType eq 'Microsoft.Compute/disks'")
+
+    def list_by_resource_group(self, resource_group, filters=None):
+        return [item for item in self.resource_mgmt_client().resources.list_by_resource_group(
+                        resource_group, filter=filters)]
+
+    def get_keeping_image_names(self, bbsrv):
+        images = list()
+        for item in bbsrv.list_blobs('sle-images'):
+            m = self.parse_image_name(item.name)
+            if m:
+                images.append(Image(item.name, flavor=m['key'], build=m['build'], date=item.properties.last_modified))
+            else:
+                logger.error("[{}] Unable to parse image name '{}'".format(self.__credentials.namespace, item.name))
+
+        return super().get_keeping_image_names(images)
+
     def cleanup_all(self):
         ''' Cleanup all autodateed data which might created during automated tests.'''
-        resourcegroup = self.cfgGet('cleanup', 'azure-storage-resourcegroup')
-        storage_account = self.cfgGet('cleanup', 'azure-storage-account-name')
-        storage_client = StorageManagementClient(self.sp_credentials(), self.subscription())
-        storage_keys = storage_client.storage_accounts.list_keys(resourcegroup, storage_account)
-        storage_keys = [v.value for v in storage_keys.keys]
-        block_blob_service = BlockBlobService(account_name='openqa', account_key=storage_keys[0])
+        block_blob_service = BlockBlobService(account_name='openqa', account_key=self.get_storage_key())
 
+        self.cleanup_bootdiagnostics(block_blob_service)
+
+        keep_images = self.get_keeping_image_names(block_blob_service)
+        self.cleanup_sle_images_container(block_blob_service, keep_images)
+        self.cleanup_disks_from_rg(keep_images)
+        self.cleanup_images_from_rg(keep_images)
+        for i in keep_images:
+            logger.info("Keep image {} ".format(i))
+
+    def cleanup_bootdiagnostics(self, block_blob_service):
         containers = block_blob_service.list_containers()
         for c in containers:
             logger.debug('Found container {}'.format(c.name))
             if (re.match('^bootdiagnostics-', c.name)):
                 self.cleanup_bootdiagnostics_container(block_blob_service, c)
-            if (c.name == 'sle-images'):
-                self.cleanup_sle_images_container(block_blob_service, c.name)
 
     def cleanup_bootdiagnostics_container(self, bbsrv, container):
         last_modified = container.properties.last_modified
@@ -100,7 +133,7 @@ class Azure(Provider):
             if (last_modified < blob.properties.last_modified):
                 last_modified = blob.properties.last_modified
         if (self.older_than_min_age(last_modified)):
-            logger.info("[Azure] Delete container {}".format(container.name))
+            logger.info("Delete container {}".format(container.name))
             if not bbsrv.delete_container(container.name):
                 logger.error("Failed to delete container {}".format(container.name))
 
@@ -141,32 +174,41 @@ class Azure(Provider):
         ]
         return self.parse_image_name_helper(img_name, regexes)
 
-    def cleanup_sle_images_container(self, bbsrv, container_name):
-        generator = bbsrv.list_blobs(container_name)
-        images = dict()
-        for img in generator:
+    def cleanup_sle_images_container(self, bbsrv, keep_images):
+        container_name = 'sle-images'
+        for img in bbsrv.list_blobs(container_name):
             m = self.parse_image_name(img.name)
-            if (m):
-                key = m['key']
-                if key not in images:
-                    images[key] = list()
-
-                logger.debug('[{}]Image {} is candidate for deletion with build {} '.format(
+            if m:
+                logger.debug('[{}] Blob {} is candidate for deletion with build {} '.format(
                     self.__credentials.namespace, img.name, m['build']))
-                images[key].append({
-                    'build': m['build'],
-                    'name': img.name,
-                    'last_modified': img.properties.last_modified,
-                })
-            else:
-                logger.error("[{}] Unable to parse image name '{}'".format(self.__credentials.namespace, img.name))
 
-        for key in images:
-            images[key].sort(key=lambda x: LooseVersion(x['build']), reverse=True)
+                if img.name not in keep_images:
+                    logger.info("Delete image '{}'".format(img.name))
+                    bbsrv.delete_blob(container_name, img.name, snapshot=None)
 
-        for img_list in images.values():
-            for i in range(0, len(img_list)):
-                img = img_list[i]
-                if (self.needs_to_delete_image(i, img['last_modified'])):
-                    logger.info("[Azure] Delete image '{}'".format(img['name']))
-                    bbsrv.delete_blob(container_name, img['name'], snapshot=None)
+    def cleanup_images_from_rg(self, keep_images):
+        rg = self.cfgGet('cleanup', 'azure-storage-resourcegroup')
+        for item in self.list_images_by_resource_group(rg):
+            m = self.parse_image_name(item.name)
+            if m:
+                logger.debug('[{}] Image {} is candidate for deletion with build {} '.format(
+                    self.__credentials.namespace, item.name, m['build']))
+
+                if item.name not in keep_images:
+                    logger.info("Delete image '{}'".format(item.name))
+                    self.compute_mgmt_client().images.delete(rg, item.name)
+
+    def cleanup_disks_from_rg(self, keep_images):
+        rg = self.cfgGet('cleanup', 'azure-storage-resourcegroup')
+        for item in self.list_disks_by_resource_group(rg):
+            m = self.parse_image_name(item.name)
+            if m:
+                logger.debug('[{}] Disk {} is candidate for deletion with build {} '.format(
+                    self.__credentials.namespace, item.name, m['build']))
+
+                if item.name not in keep_images:
+                    if self.compute_mgmt_client().disks.get(rg, item.name).managed_by:
+                        logger.warning("Disk is in use - unable delete {}".format(item.name))
+                    else:
+                        logger.info("Delete disk '{}'".format(item.name))
+                        self.compute_mgmt_client().disks.delete(rg, item.name)

ocw/lib/gce.py
@@ -1,11 +1,10 @@
 from .vault import GCECredential
-from .provider import Provider
+from .provider import Provider, Image
 import googleapiclient.discovery
 from google.oauth2 import service_account
 from dateutil.parser import parse
 import re
 import logging
-from distutils.version import LooseVersion
 
 logger = logging.getLogger(__name__)
 
@@ -109,7 +108,7 @@ class GCE(Provider):
         return self.parse_image_name_helper(img_name, regexes)
 
     def cleanup_all(self):
-        images = dict()
+        images = list()
         request = self.compute_client().images().list(project=self.__project)
         while request is not None:
             response = request.execute()
@@ -120,36 +119,25 @@ class GCE(Provider):
                 # name:sles12-sp5-gce-x8664-0-9-1-byos-build1-56
                 m = self.parse_image_name(image['name'])
                 if m:
-                    key = m['key']
-                    if key not in images:
-                        images[key] = list()
-
+                    images.append(Image(image['name'], flavor=m['key'], build=m['build'],
+                                        date=parse(image['creationTimestamp'])))
                     logger.debug('[{}]Image {} is candidate for deletion with build {}'.format(
                         self.__credentials.namespace, image['name'], m['build']))
-                    images[key].append({
-                        'build': m['build'],
-                        'name': image['name'],
-                        'creation_datetime':  parse(image['creationTimestamp']),
-                    })
                 else:
                     logger.error("[{}] Unable to parse image name '{}'".format(
                         self.__credentials.namespace, image['name']))
 
             request = self.compute_client().images().list_next(previous_request=request, previous_response=response)
 
-        for key in images:
-            images[key].sort(key=lambda x: LooseVersion(x['build']), reverse=True)
+        keep_images = self.get_keeping_image_names(images)
 
-        for img_list in images.values():
-            for i in range(0, len(img_list)):
-                img = img_list[i]
-                if (self.needs_to_delete_image(i, img['creation_datetime'])):
-                    logger.info("[GCE] Delete image '{}'".format(img['name']))
-                    request = self.compute_client().images().delete(project=self.__project, image=img['name'])
-                    response = request.execute()
-                    if 'error' in response:
-                        for e in response['error']['errors']:
-                            logger.error(e['message'])
-                    if 'warnings' in response:
-                        for w in response['warnings']:
-                            logger.error(w['message'])
+        for img in [i for i in images if i.name not in keep_images]:
+            logger.info("Delete image '{}'".format(img.name))
+            request = self.compute_client().images().delete(project=self.__project, image=img.name)
+            response = request.execute()
+            if 'error' in response:
+                for e in response['error']['errors']:
+                    logger.error(e['message'])
+            if 'warnings' in response:
+                for w in response['warnings']:
+                    logger.warning(w['message'])

ocw/lib/provider.py
@@ -3,6 +3,7 @@ import re
 from datetime import datetime
 from datetime import timedelta
 from datetime import timezone
+from distutils.version import LooseVersion
 
 
 class Provider:
@@ -50,3 +51,33 @@ class Provider:
                     'build': "-".join([gdict[k] for k in group_build if k in gdict and gdict[k] is not None]),
                 }
         return None
+
+    def get_keeping_image_names(self, images):
+        images_by_flavor = dict()
+        for img in images:
+            if (img.flavor not in images_by_flavor):
+                images_by_flavor[img.flavor] = list()
+            images_by_flavor[img.flavor].append(img)
+
+        keep_images = list()
+        for img_list in [images_by_flavor[x] for x in sorted(images_by_flavor)]:
+            img_list.sort(key=lambda x: LooseVersion(x.build), reverse=True)
+            for i in range(0, len(img_list)):
+                img = img_list[i]
+                if (not self.needs_to_delete_image(i, img.date)):
+                    keep_images.append(img.name)
+
+        return keep_images
+
+
+class Image:
+
+    def __init__(self, name, flavor, build, date, img_id=None):
+        self.name = name
+        self.flavor = flavor
+        self.build = build
+        self.date = date
+        self.id = img_id if img_id else name
+
+    def __str__(self):
+        return "[{} {} {} {}]".format(self.name, self.flavor, self.build, self.date)

tests/generators.py
@@ -15,7 +15,7 @@ class MockProperties:
 
 
 class MockImage:
-    def __init__(self, name, last_modified):
+    def __init__(self, name, last_modified=None):
         self.name = name
         self.properties = MockProperties(last_modified)
 

commit ed2c572aa105c1e838469eeb14d7cafcc279fced

Fix EC2 cleanup_all() - regression of c84755e
With c84755e I did a c&p error, it is `image[ImageId]` instead of
`image[id]` (like GCP does).

ocw/lib/EC2.py
@@ -130,7 +130,7 @@ class EC2(Provider):
                 logger.debug("[{}]Image {} is candidate for deletion with build {}".format(
                     self.__credentials.namespace, img['Name'], m['build']))
                 images.append(Image(img['Name'], flavor=m['key'], build=m['build'], date=parse(img['CreationDate']),
-                                    img_id=img['id']))
+                                    img_id=img['ImageId']))
             else:
                 logger.error("[{}] Unable to parse image name '{}'".format(self.__credentials.namespace, img['Name']))
 

tests/test_ec2.py
@@ -50,15 +50,15 @@ def test_cleanup_all(monkeypatch):
     response = {
             'Images': [
                 {'Name': 'SomeThingElse',
-                    'CreationDate': older_then_max_age, 'id': 0},
+                    'CreationDate': older_then_max_age, 'ImageId': 0},
                 {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.10.raw.xz',
-                    'CreationDate': newer_then_min_age, 'id': 1},
+                    'CreationDate': newer_then_min_age, 'ImageId': 1},
                 {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.11.raw.xz',
-                    'CreationDate': older_then_min_age, 'id': 2},
+                    'CreationDate': older_then_min_age, 'ImageId': 2},
                 {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.12.raw.xz',
-                    'CreationDate': older_then_min_age, 'id': 3},
+                    'CreationDate': older_then_min_age, 'ImageId': 3},
                 {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.13.raw.xz',
-                    'CreationDate': older_then_max_age, 'id': 4},
+                    'CreationDate': older_then_max_age, 'ImageId': 4},
                 ]
             }
     deleted_images = list()
@@ -81,9 +81,9 @@ def test_cleanup_all(monkeypatch):
     response = {
             'Images': [
                 {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.12.raw.xz',
-                    'CreationDate': older_then_min_age, 'id': 3},
+                    'CreationDate': older_then_min_age, 'ImageId': 3},
                 {'Name': 'openqa-SLES15-SP2-BYOS.x86_64-0.9.3-EC2-HVM-Build1.13.raw.xz',
-                    'CreationDate': older_then_min_age, 'id': 4},
+                    'CreationDate': older_then_min_age, 'ImageId': 4},
                 ]
             }
     ec2.cleanup_all()