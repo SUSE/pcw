c097f8bd65b91738087198e75c475929f976af3d
phoenix <felix.niederwanger@suse.com>
2022-01-24 16:26:17 +0100

    Add instances.json
    Add instances.json path to query all currently known instances as json
    object.

--- ocw/views.py
+++ ocw/views.py
@@ -5,7 +5,8 @@ from .models import Instance
 from .tables import InstanceTable
 from .tables import InstanceFilter
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.serializers import serialize
 
 
 class FilteredSingleTableView(SingleTableView):
@@ -27,6 +28,31 @@ class FilteredInstanceTableView(FilteredSingleTableView):
     model = Instance
     table_class = InstanceTable
     filter_class = InstanceFilter
+
+
+def health(request):
+    return JsonResponse({"status": "ok"})
+
+
+def instance_json(request):
+    instances = Instance.objects.all()
+    data = serialize(
+        "json",
+        instances,
+        fields=(
+            "provider",
+            "state",
+            "first_seen",
+            "last_seen",
+            "age",
+            "ttl",
+            "instance_id",
+            "region",
+            "vault_namespace",
+            "csp_info",
+        ),
+    )
+    return HttpResponse(data, content_type="application/json")
 
 
 def update(request):
