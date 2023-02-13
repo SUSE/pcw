45f96e6e12545885a6020f34eac82f60401bc278
Ricardo Branco <rbranco@suse.de>
2023-02-13 16:01:23 +0100

    Enable DEBUG if the environment variable is set

--- webui/settings.py
+++ webui/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('PCW_DEBUG', False)
 
 CONFIG_FILE = '/etc/pcw.ini'
 
