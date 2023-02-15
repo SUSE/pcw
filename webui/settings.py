1db39c65a577a97c15ce968244a99cfa00f9c4e2
Ricardo Branco <rbranco@suse.de>
2023-02-15 17:52:14 +0100

    Use a different database for authentication

--- webui/settings.py
+++ webui/settings.py
@@ -92,6 +92,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
+    },
+    'auth_db': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db', 'auth.sqlite3'),
     }
 }
 
@@ -302,3 +306,52 @@ def build_absolute_uri(path=''):
         path = '/{}'.format(path)
 
     return base_url + path
+
+
+# Taken verbatim from https://docs.djangoproject.com/en/dev/topics/db/multi-db/
+class AuthRouter:
+    """
+    A router to control all database operations on models in the
+    auth and contenttypes applications.
+    """
+    route_app_labels = {'auth', 'contenttypes'}
+
+    def db_for_read(self, model, **hints):
+        """
+        Attempts to read auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'auth_db'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write auth and contenttypes models go to auth_db.
+        """
+        if model._meta.app_label in self.route_app_labels:
+            return 'auth_db'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the auth or contenttypes apps is
+        involved.
+        """
+        if (
+            obj1._meta.app_label in self.route_app_labels or
+            obj2._meta.app_label in self.route_app_labels
+        ):
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the auth and contenttypes apps only appear in the
+        'auth_db' database.
+        """
+        if app_label in self.route_app_labels:
+            return db == 'auth_db'
+        return None
+
+
+DATABASE_ROUTERS = ['webui.settings.AuthRouter']
