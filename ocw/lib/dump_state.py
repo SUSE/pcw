85cb918e18cf2ce8cc03a59f0ae9016b3edb2580
Anton Smorodskyi <asmorodskyi@suse.de>
2023-12-21 22:02:50 +0100

    Create dedicated command to collect public cloud stats

---
+++ ocw/lib/dump_state.py
@@ -0,0 +1,21 @@
+import logging
+import traceback
+from webui.PCWConfig import PCWConfig
+from ocw.lib.azure import Azure
+from ocw.enums import ProviderChoice
+from ocw.lib.influx import Influx
+
+logger = logging.getLogger(__name__)
+
+
+def dump_state():
+    for namespace in PCWConfig.get_namespaces_for('influxdb'):
+        try:
+            providers = PCWConfig.get_providers_for('influxdb', namespace)
+            logger.info("[%s] Dump state %s", namespace, ','.join(providers))
+            if ProviderChoice.AZURE in providers:
+                Influx().dump_resource(ProviderChoice.AZURE.value, Influx.VMS_QUANTITY, Azure(namespace).list_instances)
+                Influx().dump_resource(ProviderChoice.AZURE.value, Influx.IMAGES_QUANTITY, Azure(namespace).list_images)
+                Influx().dump_resource(ProviderChoice.AZURE.value, Influx.DISK_QUANTITY, Azure(namespace).list_disks)
+        except Exception:
+            logger.exception("[%s] Dump state failed!: \n %s", namespace, traceback.format_exc())
